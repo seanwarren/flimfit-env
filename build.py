c37b81816e608102e4dbfbddab4888be82cf8de5
Sean Warren <s.warren@garvan.org.au>
2019-04-05 08:30:45 +1100

    Rename exported file with git version

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import json
 import subprocess
 import platform
 import os
+import re
 
 # Get vcpkg directory
 env_dir = os.path.dirname(os.path.realpath(__file__))
@@ -27,14 +28,22 @@ config = json.load(json_file)
 triplet_ext = ":" + config["default_triplet"][system]
 ports = [port + triplet_ext for port in config["ports"]]
 
-print("Running: " + vcpkg_dir + "vcpkg" + exec_ext)
-print("cwd: " + vcpkg_dir)
-print("pwd: " + os.getcwd())
-
 # Build ports
 subprocess.run([vcpkg_dir + "vcpkg" + exec_ext, "install"] + ports, 
    cwd=vcpkg_dir, check=True)
 
-# Export
-subprocess.run([vcpkg_dir + "vcpkg" + exec_ext, "export"] + ports + ["--zip"],
-   cwd=vcpkg_dir, check=True)
+# Export ports
+out = subprocess.check_output([vcpkg_dir + "vcpkg" + exec_ext, "export"] + ports + ["--zip"],
+         cwd=vcpkg_dir, encoding="ascii")
+print(out)
+
+# Get name of exported file
+m = re.search("Zip archive exported at: (.+)", out)
+file = os.path.realpath(m.group(1))
+
+# Create version name and rename file
+git_ver = subprocess.check_output(["git", "describe", "--abbrev=7"], encoding="ascii")
+new_file = os.path.join(env_dir, "flimfit-env-" + git_ver.strip() + ".zip")
+if os.path.exists(new_file):
+   os.remove(new_file)
+os.rename(file, new_file)
