e58e3470a1f7d43affa8e5eebe91009e95bc4236
Sean Warren <s.warren@garvan.org.au>
2019-04-06 18:10:35 +1100

    Do not export ports

--- build.py
+++ build.py
@@ -31,19 +31,4 @@ ports = [port + triplet_ext for port in config["ports"]]
 # Build ports
 subprocess.run([vcpkg_dir + "vcpkg" + exec_ext, "install", "--clean-after-build"] + ports, 
    cwd=vcpkg_dir, check=True)
-
-# Export ports
-out = subprocess.run([vcpkg_dir + "vcpkg" + exec_ext, "export"] + ports + ["--zip"],
-         cwd=vcpkg_dir, encoding="ascii", stdout=subprocess.PIPE)
-print(out.stdout)
-
-# Get name of exported file
-m = re.search("Zip archive exported at: (.+)", out.stdout)
-file = os.path.realpath(m.group(1))
-
-# Create version name and rename file
-git_ver = subprocess.check_output(["git", "describe", "--abbrev=7"], encoding="ascii").strip()
-new_file = os.path.join(env_dir, "flimfit-env-" + git_ver + ".zip")
-if os.path.exists(new_file):  
-   os.remove(new_file)
-os.rename(file, new_file)
+   
